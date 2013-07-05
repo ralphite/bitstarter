c3a5178e22b43a26666bef22de17b8799822d6af
yadongwen <ralph.wen@gmail.com>
2013-07-05 09:16:20 -0700

    updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
+var fs = require 'fs';
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buffer = fs.readFileSync('index.html');
+	var content = buffer.toString('utf-8');
+  	response.send(content);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  	console.log("Listening on " + port);
 });
