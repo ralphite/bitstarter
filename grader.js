c0e373f63418b31a66b88299fbccf7e6243a0d37
yadongwen <ralph.wen@gmail.com>
2013-07-07 02:54:59 -0700

    add new modules

--- grader.js
+++ grader.js
@@ -28,7 +28,7 @@
      - http://en.wikipedia.org/wiki/JSON
      - https://developer.mozilla.org/en-US/docs/JSON
      - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-     */
+*/
 
 var fs = require('fs');
 var program = require('commander');
